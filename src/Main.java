cad73cbb11872814b9e29bc62a947f6125ddcfa4
praneethb89 <164620936+praneethb89@users.noreply.github.com>
2026-07-15 16:30:25 +0530

    Create Main class with centralized entry point calling implementation methods

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import areacalculator.Area;
+import calculator.CalculatorClass;
+import employeebonus.Bonus;
+import stringoperations.StringOperations;
+
+public class Main {
+    public static void main(String[] args) {
+        Area.implementation();
+        CalculatorClass.implementation();
+        Bonus.implementation();
+        StringOperations.implementation();
+    }
+}
