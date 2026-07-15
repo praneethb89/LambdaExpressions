b2e02a6053f11ad81f6af20c06b95d54266b85bd
praneethb89 <164620936+praneethb89@users.noreply.github.com>
2026-07-15 16:30:40 +0530

    Add implementation method to Area class

--- src/areacalculator/Area.java
+++ src/areacalculator/Area.java
@@ -16,4 +16,12 @@ public class Area {
     public TriangleArea triangle() {
         return (base, height) -> 0.5 * base * height;
     }
+
+    public static void implementation() {
+        Area area = new Area();
+
+        System.out.println("Area of Circle: " + area.circle().calculate(7));
+        System.out.println("Area of Rectangle: " + area.rectangle().calculate(10, 5));
+        System.out.println("Area of Triangle: " + area.triangle().calculate(8, 6));
+    }
 }
