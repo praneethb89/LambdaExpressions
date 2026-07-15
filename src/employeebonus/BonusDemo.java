fc9bdd00f4ca5ddcc5206698ab5b7d86d60e5739
praneethb89 <164620936+praneethb89@users.noreply.github.com>
2026-07-15 17:03:27 +0530

    Delete src/employeebonus/BonusDemo.java

--- src/employeebonus/BonusDemo.java
+++
@@ -1,9 +0,0 @@
-package employeebonus;
-
-
-public class BonusDemo {
-    public static void main(String[] args) {
-        Bonus bonus = new Bonus();
-        System.out.println(bonus.bonusCalculator().calculateBonus(50000));
-    }
-}
