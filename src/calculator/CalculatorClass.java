5bf1334d63da9e0f24b57f378184edc90401cd94
praneethb89 <164620936+praneethb89@users.noreply.github.com>
2026-07-15 16:49:18 +0530

    Add implementation method to CalculatorClass

--- src/calculator/CalculatorClass.java
+++ src/calculator/CalculatorClass.java
@@ -19,4 +19,13 @@ public class CalculatorClass {
     public Calculator division() {
         return (x, y) -> x / y;
     }
+
+    public static void implementation() {
+        CalculatorClass calculatorClass = new CalculatorClass();
+
+        System.out.println("Addition: " + calculatorClass.addition().calculate(5, 10));
+        System.out.println("Subtraction: " + calculatorClass.subtraction().calculate(15, 10));
+        System.out.println("Multiplication: " + calculatorClass.multiplication().calculate(55, 10));
+        System.out.println("Division: " + calculatorClass.division().calculate(50, 10));
+    }
 }
